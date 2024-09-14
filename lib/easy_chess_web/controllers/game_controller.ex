3849f3e075e236421cdff413d9cc49c0e1c4e0f9
matt <matthew.hajec9@gmail.com>
2024-09-14 16:59:59 -0400

    refactored game_controller module

--- lib/easy_chess_web/controllers/game_controller.ex
+++ lib/easy_chess_web/controllers/game_controller.ex
@@ -4,26 +4,25 @@ defmodule EasyChessWeb.GameController do
   def get_game(conn, params) do
     code = params["code"]
     secret = conn.cookies["current_game_secret"]
+    role = role_from_string(conn.cookies["current_game_role"])
 
-    role =
-      if conn.cookies["current_game_role"] == "host" do
-        :host
-      else
-        :guest
-      end
+    cond do
+      !EasyChess.Lobby.lobby_exists?(code) ->
+        conn
+        |> put_flash(:error, "No lobby exists with the given code.")
+        |> redirect(to: "/")
 
-    if !EasyChess.Lobby.lobby_exists?(code) do
-      conn
-      |> put_flash(:error, "No lobby exists with the given code.")
-      |> redirect(to: "/")
-    else
-      if !EasyChess.Lobby.is_valid_secret?(code, role, secret) do
+      !EasyChess.Lobby.is_valid_secret?(code, role, secret) ->
         conn
         |> put_flash(:error, "Invalid game secret.")
         |> redirect(to: "/")
-      else
+
+      true ->
         render(conn, :game)
-      end
     end
   end
+
+
+  def role_from_string("host"), do: :host
+  def role_from_string(_), do: :guest
 end
