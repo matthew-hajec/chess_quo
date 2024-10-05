7d150747b5ea6be919f6a056f420bf4cafb8bbf0
matt <matthew.hajec9@gmail.com>
2024-10-05 14:22:46 -0400

    added error handling to get_game

--- lib/easy_chess_web/controllers/game_controller.ex
+++ lib/easy_chess_web/controllers/game_controller.ex
@@ -4,27 +4,36 @@ defmodule EasyChessWeb.GameController do
   def get_game(conn, params) do
     code = params["code"]
     secret = conn.cookies["current_game_secret"]
+    # Role is not verified, so it CAN NOT be trusted
     role = role_from_string(conn.cookies["current_game_role"])
 
     cond do
       !EasyChess.Lobby.lobby_exists?(code) ->
         conn
         |> put_flash(:error, "No lobby exists with the given code.")
+        |> put_status(404)
         |> redirect(to: "/")
 
       !EasyChess.Lobby.is_valid_secret?(code, role, secret) ->
         conn
         |> put_flash(:error, "Invalid game secret.")
+        |> put_status(403)
         |> redirect(to: "/")
 
       true ->
-        # Load the game state
-        {:ok, game} = EasyChess.Chess.GamesManager.get_game(code)
+        case EasyChess.Chess.GamesManager.get_game(code) do
+          {:error, _} ->
+            conn
+            |> put_flash(:error, "Error loading game state.")
+            |> put_status(500)
+            |> redirect(to: "/")
 
-        conn
-        |> assign(:role, role)
-        |> assign(:game, game)
-        |> render(:game)
+          {:ok, game} ->
+            conn
+            |> assign(:role, role)
+            |> assign(:game, game)
+            |> render(:game)
+        end
     end
   end
 
