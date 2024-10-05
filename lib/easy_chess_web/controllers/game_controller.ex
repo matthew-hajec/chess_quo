031d81241516634e21f8febdb6ac5413d76a1817
matt <matthew.hajec9@gmail.com>
2024-10-05 14:32:51 -0400

    added validation to get_game

--- lib/easy_chess_web/controllers/game_controller.ex
+++ lib/easy_chess_web/controllers/game_controller.ex
@@ -1,7 +1,24 @@
 defmodule EasyChessWeb.GameController do
   use EasyChessWeb, :controller
 
+  @get_game_params_schema %{
+    code: [type: :string, min_length: 8, max_length: 8]
+  }
+
   def get_game(conn, params) do
+    case Tarams.cast(params, @get_game_params_schema) do
+      {:ok, _} -> handle_get_game(conn, params)
+
+      {:error, _} ->
+        conn
+        |> put_flash(:error, "Invalid game code.")
+        |> put_status(400)
+        |> redirect(to: "/")
+    end
+  end
+
+  def handle_get_game(conn, params) do
+    IO.inspect(params)
     code = params["code"]
     secret = conn.cookies["current_game_secret"]
     # Role is not verified, so it CAN NOT be trusted
@@ -9,6 +26,8 @@ defmodule EasyChessWeb.GameController do
 
     cond do
       !EasyChess.Lobby.lobby_exists?(code) ->
+        IO.puts("No lobby exists with the given code.")
+        IO.inspect(code)
         conn
         |> put_flash(:error, "No lobby exists with the given code.")
         |> put_status(404)
