a9e27a41753d722a5119b6afcef7c48f29c086f9
matthew-hajec <matthew.hajec02@gmail.com>
2024-12-31 12:46:24 -0500

    added module for game types

---
+++ lib/chess_quo/game_types.ex
@@ -0,0 +1,9 @@
+defmodule GameTypes do
+  @moduledoc """
+  Shared types for game and lobby related modules.
+  """
+  @type color :: :white | :black
+  @type piece_type :: :pawn | :rook | :knight | :bishop | :queen | :king
+  @type player_type :: :host | :guest
+
+end
