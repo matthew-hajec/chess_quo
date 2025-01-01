cdd31ee72ec5891d11095c8feb6460dde6bf1caf
matthew-hajec <matthew.hajec02@gmail.com>
2025-01-01 18:47:37 -0500

    predefined atoms

--- lib/chess_quo/game_types.ex
+++ lib/chess_quo/game_types.ex
@@ -2,6 +2,11 @@ defmodule ChessQuo.GameTypes do
   @moduledoc """
   Shared types for game and lobby related modules.
   """
+  # Define the atoms so that String.to_existing_atom does not fail.
+  _ = [
+    :white, :black, :pawn, :rook, :knight, :bishop, :queen, :king, :host, :guest, :ongoing, :white_victory, :black_victory, :draw
+  ]
+
   @type color :: :white | :black
   @type piece_type :: :pawn | :rook | :knight | :bishop | :queen | :king
   @type player_type :: :host | :guest
