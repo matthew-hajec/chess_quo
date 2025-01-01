c96029d9f21d29388deb6b56144e1f62c20b1d1c
matthew-hajec <matthew.hajec02@gmail.com>
2025-01-01 18:53:32 -0500

    added code to ensure preloaded atoms aren't optimized out of existence

--- lib/chess_quo/game_types.ex
+++ lib/chess_quo/game_types.ex
@@ -3,9 +3,12 @@ defmodule ChessQuo.GameTypes do
   Shared types for game and lobby related modules.
   """
   # Define the atoms so that String.to_existing_atom does not fail.
-  _ = [
-    :white, :black, :pawn, :rook, :knight, :bishop, :queen, :king, :host, :guest, :ongoing, :white_victory, :black_victory, :draw
+  @preloaded_atoms [
+    :white, :black, :pawn, :rook, :knight, :bishop, :queen, :king,
+    :host, :guest, :ongoing, :white_victory, :black_victory, :draw
   ]
+  # This should never do anything, but it exists to prevent optimization.
+  Enum.each(@preloaded_atoms, fn atom -> if atom == '', do: IO.puts("") end)
 
   @type color :: :white | :black
   @type piece_type :: :pawn | :rook | :knight | :bishop | :queen | :king
